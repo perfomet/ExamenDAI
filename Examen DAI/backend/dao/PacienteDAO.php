506a5a0f5432ca4cff26c831cba4d92ceaa5eab7
david <david@smartpoint.cl>
2017-07-04 23:25:50 -0400

    ESTRUCTURA FINALIZADA, PENDIENTE CLASES MODELO, DAO Y CONTROLADOR POR MI PARTE

---
+++ Examen DAI/backend/dao/PacienteDAO.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of PacienteDAO
+ *
+ * @author david
+ */
+class PacienteDAO implements GenericDAO {
+    //put your code here
+    public function actualizar($registro) {
+        
+    }
+
+    public function agregar($registro) {
+        
+    }
+
+    public function buscarPorId($idRegistro) {
+        
+    }
+
+    public function eliminar($idRegistro) {
+        
+    }
+
+    public function listarTodos() {
+        
+    }
+
+}
